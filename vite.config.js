a7e9703f87fba407a2b9fe78c487c40c42a7d16c
sainiabhi441 <abhisheksaini0492094@gmail.com>
2025-11-22 22:48:43 +0530

    Backend API URL fixed for production

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/feedback-app/", // keep your repo name SAME
   plugins: [react()],
-  base:"/feedback-app/"
- 
+  define: {
+    "import.meta.env.VITE_API_BASE_URL": JSON.stringify(
+      "https://my-feedback-backend.onrender.com"
+    ),
+  },
 })
